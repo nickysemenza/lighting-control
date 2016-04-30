eca301c0e59c4048c648808597a1b839717a8212
Nicky Semenza <nicky@nickysemenza.com>
2016-04-30 02:57:18 -0400

    toggleable rgb fade works

--- daemon.js
+++ daemon.js
@@ -2,16 +2,25 @@ var redis = require("redis");
 var client = redis.createClient();
 var request = require('request');
 var settings = require('./settings');
+var Color = require("color");
 client.on("error", function (err) {
     console.log("Error " + err);
 });
 
+var lightsById = [];
+settings.fixtures.forEach(function(l)
+{
+	//so we can access by ID;
+	lightsById[l.id] = l;
+});
+
+var timerlist = [];
+var colorlist = [];
 function updatevals()
 {
 	var test = [];
 	client.hgetall("dmx-vals", function (err, obj) {
 		Object.keys(obj).forEach( key => {
-			// console.log(key+"-"+obj[key]);
 			test[key]=obj[key];
 		});
 		var dmx_values = test.slice(1).join(); //make comma seperated array, but ignore 0 index
@@ -22,4 +31,35 @@ function updatevals()
 				  });
 	});
 }
-setInterval(updatevals, 10);//update every 10ms
+setInterval(updatevals, 10);
+
+function advanceLightStage(light_id,mode)
+{
+	if(colorlist[light_id]==null)
+		colorlist[light_id] = Color({r: 0, g: 255, b: 255});
+	var current_color = colorlist[light_id];
+	var color_channel_map = lightsById[1].colors;
+	client.hset("dmx-vals", color_channel_map.r, current_color.red());
+	client.hset("dmx-vals", color_channel_map.g, current_color.green());
+	client.hset("dmx-vals", color_channel_map.b, current_color.blue());
+	current_color.rotate(5);
+}
+
+function lightModeWatcher()
+{
+	client.hgetall("light-modes", function (err, obj) {
+    Object.keys(obj).forEach( key => {
+    		var mode = obj[key];
+			if(!timerlist[key] && mode!="manual")
+			{
+				timerlist[key] = setInterval( function() { advanceLightStage(key,mode); }, 30 );
+			}
+			else if(timerlist[key] && mode=="manual")//switch back to manual mode
+			{
+				clearInterval(timerlist[key]);
+				timerlist[key]=null;//reset the timer
+			}
+		});
+	});
+}
+ setInterval(lightModeWatcher, 10);
